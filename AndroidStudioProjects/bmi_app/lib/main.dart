a1248d798564e189179c7a07b2d2df9fa9fe9dfa
zessu <amackzie@gmail.com>
2020-01-03 00:03:21 +0300

    (feat): whoa, this was really fast

--- AndroidStudioProjects/bmi_app/lib/main.dart
+++ AndroidStudioProjects/bmi_app/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:bmi_app/ui/home.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MaterialApp(
+void main() => runApp(MaterialApp(
       home: ScaffoldExample(),
     ));
--- AndroidStudioProjects/bmi_app/lib/ui/home.dart
+++ AndroidStudioProjects/bmi_app/lib/ui/home.dart
@@ -5,10 +5,28 @@ class ScaffoldExample extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigation menu',
+            onPressed: null,
+          ),
           title: Text("BMI Calculator"),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              tooltip: 'Search',
+              onPressed: null,
+            ),
+          ],
           centerTitle: true,
           backgroundColor: Colors.cyan),
       body: MyApp(),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Add', // used by assistive technologies
+        child: Icon(Icons.add),
+        onPressed: null,
+        backgroundColor: Colors.cyan,
+      ),
     );
   }
 }
