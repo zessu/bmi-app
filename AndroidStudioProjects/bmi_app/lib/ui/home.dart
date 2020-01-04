8c374ad6b93f3e722d4fba39b9aa35e605a78da6
zessu <amackzie@gmail.com>
2020-01-05 01:00:41 +0300

    (refactor): consolidate bottom navigation bar code

--- AndroidStudioProjects/bmi_app/lib/ui/home.dart
+++ AndroidStudioProjects/bmi_app/lib/ui/home.dart
@@ -4,39 +4,52 @@ class ScaffoldExample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: 'Navigation menu',
-            onPressed: null,
-          ),
-          title: Text("BMI Calculator"),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.search),
-              tooltip: 'Search',
+        appBar: AppBar(
+            leading: IconButton(
+              icon: Icon(Icons.menu),
+              tooltip: 'Navigation menu',
               onPressed: null,
             ),
-          ],
-          centerTitle: true,
-          backgroundColor: Colors.cyan),
-      body: MyApp(),
-      floatingActionButton: FloatingActionButton(
-        tooltip: 'Add', // used by assistive technologies
-        child: Icon(Icons.add),
-        onPressed: null,
-        backgroundColor: Colors.cyan,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text("Home")),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.history), title: Text("Previous")),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.notification_important), title: Text("Alarms")),
+            title: Text("BMI Calculator"),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.search),
+                tooltip: 'Search',
+                onPressed: null,
+              ),
+            ],
+            centerTitle: true,
+            backgroundColor: Colors.cyan),
+        body: MyApp(),
+        floatingActionButton: FloatingActionButton(
+          tooltip: 'Add', // used by assistive technologies
+          child: Icon(Icons.add),
+          onPressed: null,
+          backgroundColor: Colors.cyan,
+        ),
+        bottomNavigationBar: MyBottomNavBar());
+  }
+}
+
+class MyAppTopNavBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigation menu',
+          onPressed: null,
+        ),
+        title: Text("BMI Calculator"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          ),
         ],
-      ),
-    );
+        centerTitle: true,
+        backgroundColor: Colors.cyan);
   }
 }
 
@@ -55,5 +68,20 @@ class MyApp extends StatelessWidget {
                 fontStyle: FontStyle.italic),
           ),
         ));
+  }
+}
+
+class MyBottomNavBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.home), title: Text("Home")),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.history), title: Text("Previous")),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.notification_important), title: Text("Alarms")),
+      ],
+    );
   }
 }
